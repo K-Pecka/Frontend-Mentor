0880c6583202ea7a0901c6ae2fd69eeb693a83c2
Kacper <krgpecka@gmail.com>
2024-02-03 22:50:08 +0100

    Add image, h1, and h2 - Added image-omlette.jpeg to the assets/images folder - Added <h1> and <h2> headers in the main component

---
+++ RecipePage/src/components/FooterMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Challenge by
+    <a 
+    href="https://www.frontendmentor.io?ref=challenge" 
+    target="_blank"
+    >
+    Frontend Mentor
+    </a>
+    . Coded by <a href="#">Kacper Pecka</a>.
+  </div>
+</template>
+
+<style scoped>
+div {
+  font-size: 11px;
+  text-align: center;
+}
+div a {
+  color: hsl(228, 45%, 44%);
+}
+</style>
