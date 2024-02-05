5cd945b47bd203d21b02daf48cccf3d39f3cfbf3
Kacper <krgpecka@gmail.com>
2024-02-05 20:53:03 +0100

    Inicjalny commit - tipCalculator

---
+++ TipCalculator/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount
+        / person Total / person Reset
+      </div>
+    </>
+  );
+}
+
+export default App;
